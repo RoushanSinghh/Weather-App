783416eac5d7980aeb244e3c417c0eec240f6122
Roushan Singh <Roushankr.singh007@gmail.com>
2023-05-05 14:30:29 +0530

    forecast and geocode api added

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
 const app = express()
 
@@ -20,14 +22,14 @@ app.use(express.static(publicDirectoryPath))
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
-        name: 'Roushan Singh'
+        name: 'Andrew Mead'
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
-        name: 'Roushan Singh'
+        name: 'Andrew Mead'
     })
 })
 
@@ -35,7 +37,7 @@ app.get('/help', (req, res) => {
     res.render('help', {
         helpText: 'This is some helpful text.',
         title: 'Help',
-        name: 'Roushan Singh'
+        name: 'Andrew Mead'
     })
 })
 
@@ -46,10 +48,22 @@ app.get('/weather', (req, res) => {
         })
     }
 
-    res.send({
-        forecast: 'It is snowing',
-        location: 'Philadelphia',
-        address: req.query.address
+    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return res.send({ error })
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error })
+            }
+
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
     })
 })
 
@@ -69,7 +83,7 @@ app.get('/products', (req, res) => {
 app.get('/help/*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Roushan Singh',
+        name: 'Andrew Mead',
         errorMessage: 'Help article not found.'
     })
 })
@@ -77,7 +91,7 @@ app.get('/help/*', (req, res) => {
 app.get('*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Roushan Singh',
+        name: 'Andrew Mead',
         errorMessage: 'Page not found.'
     })
 })
---
+++ src/utils/geocode.js
@@ -0,0 +1,24 @@
+const request = require("request");
+
+const geocode = (address, callback) => {
+  const url =
+    "http://api.weatherstack.com/current?access_key=08fb793a0ce81330beae2d4299944e7d&query=" +
+    address +
+    "&limit=1";
+
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect to location services!", undefined);
+    } else if (body.location.length === 0) {
+      callback("Unable to find location. Try another search.", undefined);
+    } else {
+      callback(undefined, {
+        latitude: body.location.lat,
+        longitude: body.location.lon,
+        location: body.location.name,
+      }); 
+    }
+  });
+};
+
+module.exports = geocode;
