7c6ce43d584502aa850ade8d4c8375849ac94a8b
Roushan Singh <Roushankr.singh007@gmail.com>
2023-05-05 19:45:17 +0530

    converted degree F to C

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,13 +3,17 @@ const request = require('request')
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=08fb793a0ce81330beae2d4299944e7d&query=' + latitude + ',' + longitude + '&units=f'
 
+    const celsius = (unit) => {
+            return (unit - 30)/2
+    }
+
     request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to weather service!', undefined)
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions + ' It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.precip + '% chance of rain.')
+            callback(undefined, body.current.weather_descriptions + '. It is currently ' + celsius(body.current.temperature) + ' °C out there. There is a ' + body.current.precip + '% chance of rain.')
         }
     })
 }
